56e910a802f7ca44cb632bfbc7240a7d6f1afc22
interstate420 <interstate420@gmail.com>
2014-11-06 05:27:36 -0600

    Update to the documentation

--- check_snmp_lgpConditionsTable.php
+++ check_snmp_lgpConditionsTable.php
@@ -1,28 +1,28 @@
 #!/usr/bin/php -q
 <?php
-// check_snmp_lgpConditionsTable.php
-// Written by Tim Pratte 
-//
-// This nagios snmp2_walk scans the conditionally populated  LIEBERT-GP-CONDITIONS-MIB::lgpConditionsTable
-// for the output: "OID: LIEBERT-GP-CONDITIONS-MIB::" and prints the lgpConditionsWellKnown alert currently
-// populating the lgpConditionsDescr.* table entry.  By default, if the alert is detected but not in our "critical"
-// list, go out as a WARNING, otherwise CRITICAL
-//
-//
-// (example:  ./check_snmp_lgpConditionsTable.php 192.168.1.2 public lgpConditionsTable)
-
+/** check_snmp_lgpConditionsTable.php
+ * @author Tim Pratte
+ * @version 1.0  
+ * This nagios snmp2_walk scans the conditionally populated  LIEBERT-GP-CONDITIONS-MIB::lgpConditionsTable
+ * for the output: "OID: LIEBERT-GP-CONDITIONS-MIB::" and prints the lgpConditionsWellKnown alert currently
+ * populating the lgpConditionsDescr.* table entry.  By default, if the alert is detected but not in our "critical"
+ * list, go out as a WARNING, otherwise CRITICAL
+ *
+ *
+ * (example:  ./check_snmp_lgpConditionsTable.php 192.168.1.2 public lgpConditionsTable)
+ */
 if ($argc != 4 ) {
     echo "USAGE:\r\n";
     echo "    $argv[0] <hostname> <community> lgpConditionsTable \r\n";
     exit(2);
 }
 
-#  This is a debugging test to see if all is well ...
-#$a = array("OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionHighTemperature",
-#    "OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionCompressorHighHeadPressure",
-#    "OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionLocalAlarm1",
-#    "OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionWhatever");
-
+/**  This is a debugging test to see if all is well ...
+ *$a = array("OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionHighTemperature",
+ *    "OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionCompressorHighHeadPressure",
+ *    "OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionLocalAlarm1",
+ *    "OID: LIEBERT-GP-CONDITIONS-MIB::lgpConditionWhatever");
+ */
 $alert = " ";
 $alertprint = " ";
 $alertperf = "";
@@ -32,10 +32,10 @@ $Clist = ""; // Critical list
 $Wlist = ""; // Warning list
 $Wflag = 0;  // warning flag
 $flag = 0;
-$conditionspresent = "1.3.6.1.4.1.476.1.42.3.2.2.0"; // lgpConditionsPresent.0 OID
+$conditionspresent = "1.3.6.1.4.1.476.1.42.3.2.2.0"; /** lgpConditionsPresent.0 OID */
 $conditions = 0;
 
-#  OK, let's populate the $a array with the output from the Liebert HVAC snmpwalk
+/**  OK, let's populate the $a array with the output from the Liebert HVAC snmpwalk */
 $a = snmp2_walk($argv[1], $argv[2], $argv[3]);
 
 foreach ($a as $val) {
@@ -49,10 +49,10 @@ foreach ($a as $val) {
         $conditions = $conditions + 1;
         $flag = 0;
 
-        // If we already have a critical alert, don't reset it to warning
+        /** If we already have a critical alert, don't reset it to warning */
         if ($exitcode !== 2) { $exitprint = "WARNING:"; $exitcode = 1; }
 
-        switch ($alert) {   // this is our critical list selected from the >128 lgpConditionsTable entries
+        switch ($alert) {   /** this is our critical list selected from the >128 lgpConditionsTable entries */
             case (preg_match("/lgpConditionLocalAlarm.*/", $alert) ? true : false ): $Clist = $alert." ".$Clist; $flag = 2; break;
             case "lgpConditionHighHumidity": $Clist = $alert." ".$Clist; $flag = 2; break;
             case "lgpConditionLowHumidity": $Clist = $alert." ".$Clist; $flag = 2; break;
@@ -72,7 +72,7 @@ foreach ($a as $val) {
             case "lgpConditionSystemControlBatteryLow": $Clist = $alert." ".$Clist; $flag = 2; break;
             case "lgpConditionEmergencyShutdown": $Clist = $alert." ".$Clist; $flag = 2; break;
         }
-        // if We found a critical alert, now set the exitcode and reset the flag
+        /** if We found a critical alert, now set the exitcode and reset the flag */
         if ($flag == 2) { $exitcode = 2; $flag = 0; }
         else
         { $Wlist = $alert." ".$Wlist; $Wflag = 1; $flag = 0; }  // else add this alert to the warning list
@@ -80,18 +80,18 @@ foreach ($a as $val) {
 
 }
 
-if ($Wflag == 1 && $exitcode == 1) {   // no critical matches, so just printout the warning list
+if ($Wflag == 1 && $exitcode == 1) {   /** no critical matches, so just printout the warning list */
     echo "WARNING: $Wlist|lpgConditionsTable_alerts=$conditions $alertperf\r\n"; exit($exitcode);
 }
 
-if ($exitcode == 2 && $Wflag == 1) {    // show the critical list first, then the warnings
+if ($exitcode == 2 && $Wflag == 1) {    /** show the critical list first, then the warnings */
     echo "CRITICAL: $Clist;  WARNING: $Wlist|lpgConditionsTable_alerts=$conditions $alertperf\r\n"; exit($exitcode);
 }
 
-if ($exitcode == 2 && $Wflag == 0) { // else, just show the critical list
+if ($exitcode == 2 && $Wflag == 0) { /** else, just show the critical list */
     echo "CRITICAL: $Clist |lpgConditionsTable_alerts=$conditions $alertperf\r\n"; exit($exitcode);
 }
-#  no conditions found so exit cleanly
+/**  no conditions found so exit cleanly */
 echo "OK: lgpConditionsTable not triggered |lgpConditionsTable_alerts=$conditions\r\n";
 exit(0);
 
